183b37cbbfbc0e5eb626ef2bc2a980bb3b8d1015
jelaw21 <jelaw21@gmail.com>
2018-01-22 11:24:26 -0500

    created the Board and Game constructors

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,11 @@
-public class Board {
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel{
+
+    public Board() {
+        setPreferredSize(new Dimension(800, 600));
+        setBackground(Color.BLACK);
+    }
+    
 }
--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,33 @@
-public class Game {
+import javax.swing.*;
+
+//Inheritance - when one class "inhereits" or has access to all the methods/fields of its
+//              parent class. We use SUPER CLASS as the parent and SUB CLASS as the child.
+//              extends is the keyword that applies a superclass to a subclass.
+
+public class Game extends JFrame {
+
+    Board board;
+
+    public Game(){
+
+        setResizable(false);
+
+        //*************************
+        setVisible(true);
+        //*************************
+
+        setFocusable(true);
+        setTitle("PONG");
+        board = new Board();
+
+        add(board);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args){
+         new Game();
+    }
+
 }
