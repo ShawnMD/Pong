ea3572687ff638e858ddbc9e45e958564b2e94b3
jelaw21 <jelaw21@gmail.com>
2018-01-22 11:35:20 -0500

    created the Board and Game constructors

--- src/Board.java
+++ src/Board.java
@@ -4,7 +4,9 @@ import java.awt.*;
 public class Board extends JPanel{
 
     public Board() {
+        //sets the size JFrame.pack should use if its optimal
         setPreferredSize(new Dimension(800, 600));
+        //sets the background color of the panel
         setBackground(Color.BLACK);
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -9,24 +9,27 @@ public class Game extends JFrame {
     Board board;
 
     public Game(){
-
+        //enables/disables user ability to change frame size
         setResizable(false);
-
+        //enables/disables whether the frame renders on screen
         //*************************
         setVisible(true);
         //*************************
-
+        //enables/disables whether the frame can be an active window
         setFocusable(true);
+        //sets the text in the title bar of the frame
         setTitle("PONG");
-
+        //adds the board panel to the frame
         add(board);
-
+        //packages all components and determines best layout/size
         pack();
+        //centers the frame on the screen
         setLocationRelativeTo(null);
     }
 
     public static void main(String[] args){
-         new Game();
+        // calls a new instance of the game class - runs the constructor
+        new Game();
     }
 
 }
